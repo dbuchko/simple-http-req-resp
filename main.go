6f9343c5a630a80f904fd9df327db26670f060f5
Daniel L. Buchko <dbuchko@pivotal.io>
2019-05-07 11:59:30 -0400

    Simplified by removing logger and using simple Println().

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "fmt"
     "log"
     "os"
     "time"
@@ -11,20 +12,14 @@ const (
       DEFAULT_PORT = "8080"
 )
 
-var l = log.New(os.Stdout, "", 0)
-
-func Log(msg string) {
-        l.SetPrefix(time.Now().Format("2006-01-02 15:04:05.000") + " [INFO] ")
-        l.Print(msg)
-}
-
 func handleGet(w http.ResponseWriter, r *http.Request) {
     switch r.Method {
     case "GET":
-        Log("Received a GET request.")
+        t := time.Now()
+        fmt.Println("Received GET request at ", t.Format("2006-01-02T15:04:05.999999-07:00"))
 
     default:
-        Log("Sorry, only GET method is supported.")
+        fmt.Println("Sorry, only GET method is supported.")
     }
 }
 
@@ -32,13 +27,13 @@ func main() {
 
     var port string
     if port = os.Getenv("PORT"); len(port) == 0 {
-       log.Printf("Warning, PORT not set. Defaulting to %+v", DEFAULT_PORT)
+       fmt.Println("Warning, PORT not set. Defaulting to %+v", DEFAULT_PORT)
         port = DEFAULT_PORT
     }
 
     http.HandleFunc("/", handleGet)
 
-    Log("Starting server for testing HTTP GET...")
+    fmt.Println("Starting server for testing HTTP GET...")
     if err := http.ListenAndServe(":"+port, nil); err != nil {
         log.Fatal(err)
     }
